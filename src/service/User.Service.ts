acb847acae315cf34f6a733e728dcc9200ff3c75
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-09 22:22:17 +1100

    add validate password

--- src/service/User.Service.ts
+++ src/service/User.Service.ts
@@ -1,6 +1,8 @@
 import { DocumentDefinition} from "mongoose";
 import User, { UserDocument} from "../models/User.model";
 
+import {omit} from "lodash";
+
 /**
  *
  * @param input doesnot need createdAt, updatedAt and comparePassword from UserDocument
@@ -8,8 +10,26 @@ import User, { UserDocument} from "../models/User.model";
  */
 export async function createUser(input: DocumentDefinition<Omit<UserDocument, 'createdAt' | 'updatedAt' | 'comparePassword'>>){
     try{
-        return await User.create(input);
+        const user = await User.create(input);
+
+        return omit(user.toJSON(), "password");
+
     }catch (e: any){
         throw new Error(e)
     }
+}
+
+export async function validatePassword({ email, password}: { email: string, password: string}){
+    const user = await User.findOne({ email });
+
+    if(!user){
+        return false;
+    }
+
+    const isValidPassword = await user.comparePassword(password);
+    if(!isValidPassword){
+        return false
+    }
+
+    return omit(user.toJSON(), 'password')
 }
