ed4c4402da168423b564b08cb8389fd8c10bd346
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-23 17:58:15 +1100

    first test

---
+++ src/test/product.test.ts
@@ -0,0 +1,11 @@
+
+// @ts-ignore
+describe('Product', function () {
+    describe('Get product', () =>{
+        describe('Given', () =>{
+            it('should return a 404', function () {
+                expect(true).toBe(true)
+            });
+        })
+    })
+});
